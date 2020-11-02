4d23323eb3a1b5cb243d32f0145ed62b66f00a9f
GilangNirwana <61052215+GilangNirwana@users.noreply.github.com>
2020-11-02 13:11:23 +0700

    MAIN

---
+++ resources/views/profile/about.blade.php
@@ -0,0 +1,12 @@
+@include('profile.header')
+@include('layouts.header')
+<body class="container">
+    <div class=" bg-light p-5 mt-5 ml-3">
+        <h1 class="text-monospace" >Apotek Nusa Farma</h1>
+        <p class="lead">Apotek Nusa Darma adalah salah satu apotek terlengkap di kota Kediri yang dapat membantu memenuhi kebutuhan pengobatan Masyarakat Sekitar</p>
+        <hr>
+        <p class="font-weight-normal">Apotek Nusa Darma menyediakan berbagai macam obat yang di tangani oleh tenaga farmasi yang handal dan terpercaya</p>
+        <button type="button" class="btn btn-primary">Learn More</button>
+    </div>
+</body>
+@include('profile.footer')
---
+++ resources/views/profile/footer.blade.php
@@ -0,0 +1,3 @@
+<div class="container text-center">
+    <p>&#169;Copyright 2020</p>
+</div>
